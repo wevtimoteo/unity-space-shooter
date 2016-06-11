a8f06036f844a3659c3f42d842608199ad8cf41c
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-06-11 20:33:27 -0300

    Makes airship fires shot on hold Z key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,11 +12,25 @@ public class PlayerController : MonoBehaviour {
     public float tilt;
     public Boundary boundary;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
     private Rigidbody rb;
+    private float nextFire;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
     }
 
     void FixedUpdate ()
