4ad3428038401d5a975f51e29112d5eb1ff9dfad
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-06-12 13:54:44 -0300

    Add sound effects to game
    On shot and for explosions (player and hazards)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,9 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
+
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
         }
     }
 
